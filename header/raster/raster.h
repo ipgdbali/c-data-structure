683dc33a712236d1483e2f28aa22c9d64022f23e
I Putu Gede Darmawan S.Kom <ipgd_bali@yahoo.com>
2019-08-19 13:19:45 +0800

    Removing raster.h

--- header/raster/raster.h
+++
@@ -1,9 +0,0 @@
-#ifndef RASTER_H
-#define RASTER_H
-
-extern void draw_line(struct tBitMap *,struct tPoint[1],struct tPoint[1],struct tColor[1],struct tColor[1]);
-extern void draw_hline(struct tBitMap *,int y,int x1,int x2);
-extern void fill_triangle(struct tBitMap *,struct tPoint[3],struct tColor *);
-extern void fill_triangle(struct tBitMap *,struct tPoint[3],struct tColor[3]);
-
-#endif
